b223abf5dc5d8b270751ee9df5ceacb480fb3f26
Divya-1D <divyapanchal0195@gmail.com>
2022-05-11 16:20:06 -0500

    way to finishing

--- assignment2.js
+++ assignment2.js
@@ -1,4 +1,4 @@
-// Q : 03
+//Data
 usersName = [
 	{
 		"name":"Harry Potter",
@@ -44,33 +44,95 @@ async function getNamesFromApi() {
 getNamesFromApi();
 
 
-//Display user name from city London
+//Display user name from city London : (In console)
 var city_name = usersName.filter(function(cname) {
 	return cname.city == "London";
 });
 console.log(city_name)
 
 
+
+
 //1.
+const url1 = 'https://jsonplaceholder.typicode.com/posts';
 
-const url = 'https://jsonplaceholder.typicode.com/posts';
+const answer1 = document.querySelector('.answer1')
+async function getAllUsersById1(id, url) {
+    const response1 = await fetch(url);
+    const data1 = await response1.json();
+    const result1 = data1.filter(element => {
+        return element.userId === id}).forEach(element => {
+            const li = document.createElement('li');
+            li.appendChild(document.createTextNode(`${element.title}\n`))
+            answer1.appendChild(li);
+        });    
+}
+getAllUsersById1(1, url1)
 
-//api calls inside async function and await for the response
-async function getAllCountriesIdFromApi() {
 
-    // Make api call .. using await
-    const response = await fetch(url);
 
-    // converts to json data
-    const data1 = await response.json();
+//2.
+//1).Products Name and Price, whose price is greater than 100
+const url2 = 'https://fakestoreapi.com/products';
+
+
+async function getNameAndPrice(url){
+    const response2_1 = await fetch(url);
+    const data2_1 = await response2_1.json();
+    const answer2_1 = document.querySelector('.answer2_1')
+
+
+    const result2 = data2_1.filter(element => {
+        return element.price > 100}).forEach(element => {
+            const li = document.createElement('li');
+            li.appendChild(document.createTextNode(`Product Name:${element.title},\tPrice:${element.price}`));
+            answer2_1.appendChild(li);
+        })
     
-    console.log("[");
-    data1.forEach((country) => {
+}
 
-        if(country.userId=="1"){
-        console.log(country);
-        }
+getNameAndPrice(url2)
+
+
+
+
+
+//1). Display all cats name is decending order
+const url3 = 'https://api.thecatapi.com/v1/breeds';
+
+async function catsNameInDescending(url) {
+    const answer3_1 = document.querySelector('.answer3_1');
+    const response3_1 = await fetch(url);
+    const data3_1 = await response3_1.json();
+
+
+    const catsDecending = data3_1.map(element => {
+        return element.name
+    }).sort().reverse().forEach(element => {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(element));
+        answer3_1.appendChild(li);
+    });
+
+}
+catsNameInDescending(url3)
+
+
+
+//2). Get the Cats with country-code=US
+async function catsFromUS(url, countryCode) {
+    const answer3_2 = document.querySelector('.answer3_2');
+    const response3_2 = await fetch(url);
+    const data3_2 = await response3_2.json();
+
+    //uses filter to get the cats whose country_code is US
+    const US_Cats = data3_2.filter(element => {
+        return element.country_codes === countryCode;
+    }).forEach(element => {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(element.name));
+        answer3_2.appendChild(li);
     })
-    console.log("]");
- }
- getAllCountriesIdFromApi();
+}
+catsFromUS(url3, 'US')
+
